295260a80a774527ba8c6c8bdb872385c75e84da
SpencerCragun <spencer.cragun@gmail.com>
2024-10-28 17:26:14 -0700

    explore puppy arrays

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import { puppyList } from './data'
+import { useState } from 'react'
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
 
   return (
     <>
       <div>
+        {
+           puppies.map((puppy) => {
+            return <p>{puppy.name}</p>
+          })
+        }
       </div>
     </>
   )
